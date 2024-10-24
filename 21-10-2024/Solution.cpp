8a4f0099bf71e87f46bffdfabe86aad6832cfbf8
Madhurima Rawat <105432776+madhurimarawat@users.noreply.github.com>
2024-10-24 20:34:05 +0530

    Added Solution and Output in C++
    Added Solution and Output in C++

---
+++ 21-10-2024/Solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+*
+* *
+* * *
+* * * *
+
+*/
+
+int main()
+{
+    int n;
+    cout << "Enter number of Rows: ";
+    cin >> n;
+
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
